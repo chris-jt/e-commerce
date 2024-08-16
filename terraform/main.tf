e9dca90a93b8cc71d7186c84ad34fe51f05f07e3
chris-jt <chtronconi@gmail.com>
2024-08-16 14:15:07 -0300

    cambios main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,5 @@
 # Proveedor AWS
+# Proveedor AWS
 provider "aws" {
   region = var.region
 }
@@ -7,20 +8,19 @@ provider "aws" {
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "5.0.0"
-
+  
   name = "ecommerce-vpc"
   cidr = "10.0.0.0/16"
-
+  
   azs             = ["${var.region}a", "${var.region}b", "${var.region}c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  enable_nat_gateway = true
-  single_nat_gateway = true
-
+  
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
   enable_dns_hostnames = true
   enable_dns_support   = true
-
+  
   tags = {
     Environment = "dev"
     Project     = "E-commerce-Project"
@@ -29,21 +29,21 @@ module "vpc" {
 
 # Módulo EKS
 module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  version         = "18.20.0"
+  source  = "terraform-aws-modules/eks/aws"
+  #version = "18.20.0"
   
   cluster_name    = var.cluster_name
   cluster_version = "1.28"
   
-  vpc_id          = module.vpc.vpc_id
-  subnet_ids      = module.vpc.private_subnets
-
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+  
   # EKS Managed Node Group(s)
   eks_managed_node_group_defaults = {
     disk_size      = 50
-    instance_types = ["t2.micro"]
+    instance_types = ["t3.small"]  # Cambiado de t2.micro a t3.small
   }
-
+  
   eks_managed_node_groups = {
     default = {
       min_size     = 1
@@ -51,13 +51,23 @@ module "eks" {
       desired_size = 2
     }
   }
-
+  
   # aws-auth configmap
   manage_aws_auth_configmap = true
-
+  
   tags = {
     Environment = "dev"
     Project     = "E-commerce-Project"
   }
 }
 
+# Proveedor de Kubernetes
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.this.token
+}
+
+data "aws_eks_cluster_auth" "this" {
+  name = module.eks.cluster_name
+}
