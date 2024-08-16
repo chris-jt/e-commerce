8b048cbb87c2e68aed6d15d9cb6d69e5b0bccdca
chris-jt <chtronconi@gmail.com>
2024-08-16 09:01:32 -0300

    se actualiza version k8s

--- terraform/main.tf
+++ terraform/main.tf
@@ -33,7 +33,7 @@ module "eks" {
   version         = "18.20.0"
   
   cluster_name    = var.cluster_name
-  cluster_version = "1.21"
+  cluster_version = "1.28"
   
   vpc_id          = module.vpc.vpc_id
   subnet_ids      = module.vpc.private_subnets
